0a799c1076d8431e70e6d65a90692e810bd74d09
tanyakalianda <42977166+tanyakalianda@users.noreply.github.com>
2018-12-20 11:14:57 -0500

    Add files via upload

--- src/MainClass.java
+++ src/MainClass.java
@@ -8,28 +8,55 @@ import java.util.Scanner;
  */
 public class MainClass 
 {
+	
 	public static void main(String[] args)
 	{
+		final double OVER_DRAFT_FEE = 15;
+		final double RATE = 0.0025;
+		final double TRANSACTION_FEE = 1.5;
+		final double MIN_BAL = 300;
+		final double MIN_BAL_FEE = 10;
+		final int FREE_TRANSACTIONS = 10;
+		
 		ArrayList<BankAccount> accounts = new ArrayList<BankAccount>();
-		accounts.add(new CheckingAccount("Tanya", 15, 1.5, 10));
-		accounts.add(new SavingsAccount("Tanya", 0.025, 300, 10));
+	
 		
 		boolean terminate = false;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter \"add\" to add an account, enter \"transaction\" to make a transaction, or enter \"end\" to terminate the program: ");
+		String answer = in.nextLine();
+		
 		while (!terminate)
 		{
-			Scanner in = new Scanner();
-			System.out.print("Enter \"add\" to add an account, enter \"transaction\" to make a transaction, or enter \"terminate\" to terminate the program: ");
-			String answer = in.nextLine();
 			if (answer.equals("add"))
 			{
-				
+				System.out.print("Enter \"checking\" to create a checking account or \"savings\" to creaate a savings account: ");
+				String account = in.nextLine();
+				while(!account.equals("checking") && !account.equals("savings"))
+				{
+					System.out.print("Invalid input. Please try again: ");
+					account = in.nextLine();
+				}
+				if (account.equals("checking"))
+				{
+					System.out.print("Enter the name of the account: ");
+					String name = in.nextLine();
+					accounts.add(new CheckingAccount(name, OVER_DRAFT_FEE, TRANSACTION_FEE, FREE_TRANSACTIONS));
+				}
+				else if (account.equals("savings"))
+				{
+					System.out.print("Enter the name of the account: ");
+					String name = in.nextLine();
+					accounts.add(new SavingsAccount(name, RATE, MIN_BAL, MIN_BAL_FEE));
+				}
 			}
 			else if (answer.equals("transaction"))
 			{
 				
 			}
-			else if (answer.equals("terminate"))
+			else if (answer.equals("end"))
 			{
+				System.out.println("Program ended");
 				terminate = true;
 			}
 			else
