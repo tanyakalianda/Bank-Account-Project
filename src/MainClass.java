9c0bd6a1e0e437e67851859c76148fd60087c900
tanyakalianda <42977166+tanyakalianda@users.noreply.github.com>
2019-01-01 23:36:49 -0500

    Add files via upload

--- src/MainClass.java
+++ src/MainClass.java
@@ -52,37 +52,89 @@ public class MainClass
 			}
 			else if (answer.equals("transaction"))
 			{
-				System.out.print("Enter \"w\" to withdraw money, \"d\" to deposit money, \"t\" to transfer money, or \"num\" to get the account number: ");
-				String response = in.nextLine();
-				switch(response) 
+				//find my account and store it in a local variable
+				System.out.print("Please enter your account number: ");
+				int num = in.nextInt();
+				in.nextLine();
+				boolean accountNum = true;
+				while (true)
 				{
-					case "w":
+					BankAccount number = null;
+					for (int i = 0; i < accounts.size(); i++)
 					{
-						//find my account and store it in a local variable
-						System.out.print("Please enter your account number: ");
-						int num = in.nextInt();
-						in.nextLine();
-						BankAccount number = null;
-						for (int i = 0; i < accounts.size(); i++)
+						if (accounts.get(i).getAccountNumber() == num)
 						{
-							if (accounts.get(i).getAccountNumber() == num)
+							number = num;
+						}
+					}
+					if (number == null)
+					{
+						System.out.print("This is not a valid account number");
+						System.out.print("Please type \"reenter\" if you would like to reenter your account number or type \"name\" to get your account number: ");
+						String ans = in.nextLine();
+						boolean tryAgain = true;
+						while (tryAgain)
+						{
+							if (ans.equals("reenter"))
 							{
-								number = num;
+								System.out.print("Enter your account number: ");
+								num = in.nextInt();
+								in.nextLine();
+								tryAgain = false;
+							}
+							else if (ans.equals("name"))
+							{
+								System.out.print("Enter your name: ");
+								String name = in.nextLine();
+								//must return all the accounts with this name
+								tryAgain = false;
+							}
+							else
+							{
+								System.out.print("Invalid input. Please try again: ");
+								ans = in.nextLine();
 							}
 						}
-						if (number == null)
-						{
-							System.out.print("This is not a valid account number. Please try again: ");
-						}
-						System.out.print("Enter the amount you wish to withdraw: ");
-						double amount = in.nextDouble();
-						accounts.withdraw(amount);
 					}
-					case "d":
+					else
 					{
-						
+						System.out.print("Enter \"w\" to withdraw money, \"d\" to deposit money, \"t\" to transfer money, or \"num\" to get the account number: ");
+						String response = in.nextLine();
+						switch(response) 
+						{
+							case "w":
+							{
+								System.out.print("Enter the amount you wish to withdraw: ");
+								double amount = in.nextDouble();
+								//must identify which account you want to use
+								number.withdraw(amount);
+								accountNum = false;
+							}
+							case "d":
+							{
+								System.out.print("Enter the amount you wish to deposit: ");
+								double amount = in.nextDouble();
+								number.deposit(amount);
+								accountNum = false;
+							}
+							case "t":
+							{ //must det which account the money is being transfered to
+								System.out.print("Enter the amount you wish to transfer: ");
+								double amount = in.nextDouble();
+								in.nextLine();
+								System.out.print("Enter the account number you wish to transfer the money to: ");
+								int account = in.nextInt();
+								in.nextLine();					
+								number.transfer(account, amount);
+								accountNum = false;
+							}
+							case "num":
+							{
+								System.out.print(number.getAccountNumber());
+								accountNum = false;
+							}
+						}
 					}
-					
 				}
 			}
 			else if (answer.equals("end"))
@@ -95,15 +147,7 @@ public class MainClass
 				System.out.print("Invalid input. Please try again: ");
 				answer = in.nextLine();
 			}
-		}
-		
-		//must create a getAccountNumber method in the bank account class (abstract)
-		
-		//must use letters (strings), not numbers
-		//ie type "a" for transfer
-	
-		
-
+		}	
 	}
 
 }
